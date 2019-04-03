41fdf52167437847182bfb0230b4cc8910f26c24
Jisk Attema <j.attema@esciencecenter.nl>
2019-04-03 09:41:55 +0200

    fix ampersands in combined corpus

--- labelled/combined/make_combined.sh
+++ labelled/combined/make_combined.sh
@@ -5,3 +5,10 @@ cat \
   nl_alpino-ud-dev.conllu \
   nl_alpino-ud-train.conllu |
 python3  ./check_sentences.py > make_combined.out 2> make_combined.err
+
+
+# replace "&amp;" with a regular ampersand
+for f in dataset_test.conllu dataset_train.conllu dataset_dev.conllu; do
+  mv "$f" fix
+  cat fix | sed 's/&amp;/\&/g' > "$f"
+done
